7d51139f3a66f5ac6c5ba7387af4c7591fa559cf
LaurentBvr <biver.laurent@live.be>
2026-05-13 11:17:25 +0200

    feat: SearchBar dans navbar avec dropdown live (debounce 300ms)

--- frontend/src/app/core/components/navbar/navbar.ts
+++ frontend/src/app/core/components/navbar/navbar.ts
@@ -2,22 +2,21 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AsyncPipe } from '@angular/common';
 import { AuthService } from '../../services/auth';
+import { SearchBar } from '../../../shared/search-bar/search-bar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, AsyncPipe],
+  imports: [RouterLink, RouterLinkActive, AsyncPipe, SearchBar],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
   auth = inject(AuthService);
-
   getInitiales(user: any): string {
-  if (!user) return '';
-  const p = user.prenom?.charAt(0) ?? '';
-  const n = user.nom?.charAt(0) ?? '';
-  return (p + n).toUpperCase();
-}
-  
+    if (!user) return '';
+    const p = user.prenom?.charAt(0) ?? '';
+    const n = user.nom?.charAt(0) ?? '';
+    return (p + n).toUpperCase();
+  }
 }
